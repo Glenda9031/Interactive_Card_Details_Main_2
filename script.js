0f422ecfa4aa5e8655b620631a896f839485aabd
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2024-03-03 17:10:24 -0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const cardBackCode = document.querySelector('.card-back-code');
+const cardFrontCode = document.querySelector('.card-frront-code');
+const cardName = document.querySelector('.card-name');
+const expirationDateMonth = document.querySelector('.mm');
+const expirationDateYear = document.querySelector('.yy');
+
+const cardBackCodeInput = document.getElementById('cvc');
+const cardFrontCodeInput = document.getElementById('card-number');
+const cardNameInput = document.getElementById('name');
+const expirationDateMonthInput = document.getElementById('mm');
+const expirationDateYearInput = document.getElementById('yy');
+
